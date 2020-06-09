fc11e90b966dce983abfd398c63cd1de7a393125
ntheanhbk <ntheanhbk@gmail.com>
2020-06-09 10:47:05 +0700

    Add Timer/Counter 0

---
+++ TC0/1ms/1ms.c
@@ -0,0 +1,29 @@
+#include<avr/io.h>
+#include<avr/interrupt.h>
+
+//Dinh thoi 1ms voi Timer/Counter 0
+
+int main(){
+	DDRB |= 0xFF;
+	PORTB = 0x00;
+	
+	//Chon tan so clk/8 (prescaler = 8) -> T = 8us -> max t = 8us * 256 = 2048us > 1ms
+	TCCR0 = (1 << CS01);
+	//Dinh thoi 1ms = 1000us -> bo dem can dem 125 lan de het 1ms -> gia tri khoi dau la: 256 - 125 = 131
+	TCNT0 = 131;
+	//Cho phep ngat khi co co tran o T/C0
+	TIMSK = (1 << TOIE0);  
+	sei(); //set bit I cho phep ngat toan cuc
+
+	while(1){
+		
+	}
+
+	return 0;
+}
+
+//Trinh phuc vu ngat tran T/C0
+ISR (TIMER0_OVF_vect){
+	TCNT0 = 131;
+	PORTB ^= 1;
+}
---
+++ TC0/README.md
@@ -0,0 +1,1 @@
+<img alt="TC0 simulation" src="./Assets/TC0.gif"/>
